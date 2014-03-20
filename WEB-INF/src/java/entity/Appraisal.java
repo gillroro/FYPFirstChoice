38654f874098663ab225ad32e190dbb8709e6d2d
Gillian Rowan <gillroro@gmail.com>
2014-03-20 09:19:38 +0000

    updated

---
+++ WEB-INF/src/java/entity/Appraisal.java
@@ -0,0 +1,61 @@
+package entity;
+
+public class Appraisal {
+	
+	private String accomplishments;
+	private String performance;
+	private String barriers;
+	private String improvements;
+	private String attendanceRecord;
+	private String respectRecord;
+	private Employee employee;
+	
+	
+	public String getAccomplishments() {
+		return accomplishments;
+	}
+	public void setAccomplishments(String accomplishments) {
+		this.accomplishments = accomplishments;
+	}
+	public String getPerformance() {
+		return performance;
+	}
+	public void setPerformance(String performance) {
+		this.performance = performance;
+	}
+	public String getBarriers() {
+		return barriers;
+	}
+	public void setBarriers(String barriers) {
+		this.barriers = barriers;
+	}
+	public String getImprovements() {
+		return improvements;
+	}
+	public void setImprovements(String improvements) {
+		this.improvements = improvements;
+	}
+	public String getAttendanceRecord() {
+		return attendanceRecord;
+	}
+	public void setAttendanceRecord(String attendanceRecord) {
+		this.attendanceRecord = attendanceRecord;
+	}
+	public String getRespectRecord() {
+		return respectRecord;
+	}
+	public void setRespectRecord(String respectRecord) {
+		this.respectRecord = respectRecord;
+	}
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+	
+	
+	
+
+}
